ae5b447b32ccea54f8a7ee984e802c0a2fc36957
shuao23 <shuao23@gmail.com>
2017-02-05 10:59:16 -0600

    Initial commit

---
+++ app/src/main/java/ipro239/iitbeaconproject/TheBeaconApp.java
@@ -0,0 +1,13 @@
+package ipro239.iitbeaconproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TheBeaconApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_beacon_app);
+    }
+}
