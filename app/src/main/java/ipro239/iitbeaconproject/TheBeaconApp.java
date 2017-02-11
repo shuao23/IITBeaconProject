21120a4c133d3ff266f1304c7fc1f010e14c037e
Chang Liu <cliu120@hawk.iit.edu>
2017-02-10 23:55:36 -0600

    2/10

--- app/src/main/java/ipro239/iitbeaconproject/TheBeaconApp.java
+++ app/src/main/java/ipro239/iitbeaconproject/TheBeaconApp.java
@@ -1,13 +1,25 @@
 package ipro239.iitbeaconproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TheBeaconApp extends AppCompatActivity {
+    private Button btnLogin,btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_the_beacon_app);
+        setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TheBeaconApp.this,CreateAccount.class));
+            }
+        });
     }
 }
