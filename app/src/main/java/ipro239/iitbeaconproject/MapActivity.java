d9510ef06101756e1c4c0139fb9db4cb30d13e3a
Shuhei Aoki <shuao23@gmail.com>
2017-03-01 14:59:38 -0600

    added beacon library but doesnt work

--- app/src/main/java/ipro239/iitbeaconproject/MapActivity.java
+++ app/src/main/java/ipro239/iitbeaconproject/MapActivity.java
@@ -2,14 +2,31 @@ package ipro239.iitbeaconproject;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import com.qozix.tileview.TileView;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.logging.LogManager;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 
+public class MapActivity extends AppCompatActivity implements BeaconConsumer, MonitorNotifier {
+
+    private static final int PERMISSION_REQUEST_BLE = 376;
+    private static final int PERMISSION_REQUEST_LOCATION = 913;
+
+    private BackgroundPowerSaver powerSaver;
+    private BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +39,36 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
 
         //Beacon Setup
-
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+        beaconManager.bind(this);
     }
 
+    @Override
+    public void onBeaconServiceConnect() {
+        Identifier uuid = Identifier.parse(getResources().getString(R.string.eddystone_namespace));
+        Region testRegion = new Region("test_region", null, null, null);
+        beaconManager.addMonitorNotifier(this);
+        try{
+            beaconManager.startMonitoringBeaconsInRegion(testRegion);
+        }catch (RemoteException e){
+            e.printStackTrace();
+        }
+        powerSaver = new BackgroundPowerSaver(this);
+    }
+
+    @Override
+    public void didEnterRegion(Region region) {
+        Toast.makeText(this, region.getId2() + " Entered", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+        Toast.makeText(this, region.getId2() + " Exited", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+
+    }
 }
